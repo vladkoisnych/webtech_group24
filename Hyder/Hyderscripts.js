0f8cb080f03a22b7c308fe86cfc57103af17e141
Coding-8122 <hyderahussain@gmail.com>
2025-04-11 13:21:13 +0100

    adding in counter placement functionality

--- Hyder/Hyderscripts.js
+++ Hyder/Hyderscripts.js
@@ -6,19 +6,35 @@
     ctx.fillStyle= "#376996";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-function handleClick(event){
+function boardClick(event){
     console.log("You have clicked the board!");
 }
-canvas.addEventListener("click",handleClick);
+canvas.addEventListener("click",boardClick);
 
 
 let board = [];
     for(let row = 0; row < 6; row++) {
         board[row] = [];
-        for (let column = 0; column < 7; column ++) {
+        for (let column = 0; column < 7; column++) {
             board[row][column] = null;
         }
     }
-console.log("Script is running");
-console.log(board);
+
+    console.log(board);
+
+function insertCounter(counter,player){
+    console.log("Insert your counter!");
+}
+
+
+if(board[row][column] == null){
+console.log("Valid placement");
+}
+else {
+console.log("invalid placement");
+}
+
+canvas.addEventListener("click",insertCounter);
+
+
 
